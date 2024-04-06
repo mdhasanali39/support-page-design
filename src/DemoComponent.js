70131e83a6d4f0b048d67a96845685ae94a1601a
mdhasanali39 <contact.hasankhanyt@gmail.com>
2024-04-06 11:39:40 +0600

    basic setup completed

---
+++ src/DemoComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DemoComponent = () => {
+  return (
+    <div className="">
+      hello world
+    </div>
+  );
+}
+
+export default DemoComponent;
---
+++ src/components/FaqTopic/FaqTopic.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FaqTopic = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default FaqTopic;
