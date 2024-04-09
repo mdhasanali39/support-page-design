4ce06d9395c20b2e0b2445c8a734471e450fc538
mdhasanali39 <contact.hasankhanyt@gmail.com>
2024-04-09 16:25:32 +0600

    FaqQuestions updated with added tooltip on hover at menu collaps/expand

--- src/components/FaqSection/FaqQuestions/FaqQuestions.js
+++ src/components/FaqSection/FaqQuestions/FaqQuestions.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { faqTopicData } from "../../../fakeData/fakeData";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleQuestion, faCaretLeft } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCircleQuestion,
+  faCaretLeft,
+} from "@fortawesome/free-solid-svg-icons";
 
 const FaqQuestions = ({ selectedTopicName, isExpand, setIsExpand }) => {
   const [selectedTopic, setSelectedTopic] = useState({});
+  const [isTooltip, setIsTooltip] = useState(false);
 
   useEffect(() => {
     setSelectedTopic(
@@ -13,7 +17,13 @@ const FaqQuestions = ({ selectedTopicName, isExpand, setIsExpand }) => {
   }, [selectedTopicName]);
 
   return (
-    <div className={`bg-white ${isExpand ? "w-0 opacity-0 pointer-events-none lg:opacity-100 lg:pointer-events-auto lg:w-3/4 xl:w-3/5":"w-full"} p-8 border relative mr-4`}>
+    <div
+      className={`bg-white ${
+        isExpand
+          ? "w-0 opacity-0 pointer-events-none lg:opacity-100 lg:pointer-events-auto lg:w-3/4 xl:w-3/5"
+          : "w-full"
+      } p-8 border relative mr-4`}
+    >
       <h2 className="text-lg">{selectedTopicName}</h2>
       <ul className="space-y-3 mt-4">
         {selectedTopic &&
@@ -29,9 +39,23 @@ const FaqQuestions = ({ selectedTopicName, isExpand, setIsExpand }) => {
             </li>
           ))}
       </ul>
-      <div className={`absolute ${isExpand ? "-left-3": "-left-1"} -top-[6px] select-none`}>
-            <FontAwesomeIcon onClick={()=>setIsExpand(!isExpand)} icon={faCaretLeft} className={`text-3xl text-blue-500 cursor-pointer ${!isExpand && "-rotate-180"}`} />
-            </div>
+      <div
+        className={`absolute ${
+          isExpand ? "-left-3" : "-left-1"
+        } -top-[6px] select-none`}
+      >
+        <FontAwesomeIcon
+          onClick={() => setIsExpand(!isExpand)}
+          onMouseEnter={()=>setIsTooltip(true)}
+          onMouseLeave={()=>setIsTooltip(false)}
+          icon={faCaretLeft}
+          className={`text-3xl text-blue-500 cursor-pointer ${
+            !isExpand && "-rotate-180"
+          }`}
+        />
+      </div>
+      {/* tooltip */}
+      <p className={`absolute -top-[3.5%] left-0 ${isTooltip?"opacity-100 pointer-events-auto":"opacity-0 pointer-events-none"} text-sm bg-blue-200 px-3 py-1 rounded-lg transition duration-100`}>{isExpand? "Collapse":"Expand"}</p>
     </div>
   );
 };
