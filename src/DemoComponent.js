2e2d1a315444f38c330cbe8fefcdf2df3c928847
mdhasanali39 <contact.hasankhanyt@gmail.com>
2024-04-09 15:48:25 +0600

    DemoComponent updated with added footer and fakeData updated

--- src/DemoComponent.js
+++ src/DemoComponent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import FaqSection from './components/FaqSection/FaqSection';
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 const DemoComponent = () => {
   return (
     <div>
       <Navbar/>
       <FaqSection/>
+      <Footer/>
     </div>
   );
 }
